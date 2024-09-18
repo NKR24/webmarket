2f8d4b7c5961e876d3a6c4e313c12973756affb5
Nikita Khvorostin <khvorostinn@gmail.com>
2024-09-19 01:21:06 +0300

    added .env

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+
 	"market/internal/db"
 	"market/internal/elastic"
 	"market/internal/kafka"
 	"market/internal/models"
-	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	// Подключаем PostgreSQL
-	db, err := db.Connect(os.Getenv("POSTGRES_DSN"))
+	// Загружаем переменные окружения из файла .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Получаем параметры подключения к PostgreSQL из переменных окружения
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbPassword := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+	dbHost := os.Getenv("POSTGRES_HOST")
+	dbPort := os.Getenv("POSTGRES_PORT")
+	// Формируем строку подключения к базе данных
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	// Подключаемся к PostgreSQL
+	db, err := db.Connect(dsn)
 	if err != nil {
 		log.Fatal("Failed to connect to PostgreSQL:", err)
 	}
@@ -19,8 +38,11 @@ func main() {
 	// Подключаем Kafka Producer
 	kp := kafka.NewProducer(os.Getenv("KAFKA_BROKER"), "products")
 
+	p := new(models.Product)
 	// Создаем продукт и отправляем событие в Kafka
-	p := &models.Product{Name: "Product 1", Description: "A test product", Price: 19.99}
+	p.Name = "Product 1"
+	p.Description = "A test product"
+	p.Price = 19.99
 	_, err = db.CreateProductWithKafka(p, kp)
 	if err != nil {
 		log.Fatal("Failed to create product:", err)
